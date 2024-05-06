d2b7ac9b24e38b0c79b80b5365166fb01f9d883a
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-05-06 17:01:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Certificet from './Certificet'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <Certificet/>
+   </>
+  )
+}
+
+export default App
